20afcb5ff65c86e3f6ede4ce97baf1117ad04487
David Ka <david.karnel@student.kyh.se>
2022-03-16 13:09:11 +0100

    Updated info

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main extends NbDecoder{
 
     public static void main(String[] args) {
 
-        //String str-old = "02060004A30B00F6B5690800F80003000220060305E661";
+        //String str-old-v2-6 = "02060004A30B00F6B5690800F80003000220060305E661";
 
         NbDecoder nb = new NbDecoder();
         nb.decoder("02040004A30B00F60800F802202203101144220003000220");
